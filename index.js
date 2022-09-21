a43534bdf040b08faa794d215001a6da9b9166c0
andrewmulya22 <56527292+andrewmulya22@users.noreply.github.com>
2022-09-21 14:25:23 +0900

    add route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Response Send");
+});
+
 app.listen(PORT, () => {
   console.log("server started");
 });
